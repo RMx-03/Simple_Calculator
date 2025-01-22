1dbeb52524bb28aed418aee5c5b8651d892b16fc
RMx-03 <140522220+RMx-03@users.noreply.github.com>
2025-01-22 13:46:40 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display').value;
+    document.getElementById('display').value = display.substring(0, display.length - 1);
+}
+
+function calculateResult() {
+    let result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
+}
